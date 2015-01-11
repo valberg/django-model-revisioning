5f497199704b3d2f0ebd39a25e25eeed610a2e9c
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-12 00:24:45 +0100

    Docs stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,6 @@
 import sys
 import os
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -107,7 +103,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+
+if os.environ.get('READTHEDOCS', None):
+    html_theme = 'default'
+else:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
