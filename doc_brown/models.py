82cdd2b8ad8c01a3ef6e2d813eb6b9a297eb59e6
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-18 17:20:33 +0100

    Revisions have their own model, parent_revision, current_revision

--- doc_brown/models.py
+++ doc_brown/models.py
@@ -8,17 +8,40 @@ from django.db.models.base import ModelBase
 from django.utils import six
 
 
+class Revision(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.is_head = True
+        if kwargs.pop('remove_head', None):
+            self.is_head = False
+        else:
+            self.update_head()
+        super(Revision, self).save(*args, **kwargs)
+
+    @classmethod
+    def update_head(cls):
+        try:
+            current_head = cls.objects.get(is_head=True)
+            current_head.save(remove_head=True)
+        except cls.DoesNotExist:
+            pass
+
+    def __str__(self):
+        return str(self.id)
+
+
 def revision_on_delete(collector, field, sub_objs, using):
     serialized = serializers.serialize('json',
                                        [getattr(sub_objs[0], field.name)])
-
     for obj in sub_objs:
         setattr(obj, field.name, None)
         setattr(obj, field.name + '_serialized', serialized)
         obj.save()
 
 
-def auto_now_field(field):
+def is_auto_now_field(field):
     if isinstance(field, models.DateField) or\
             isinstance(field, models.DateTimeField):
         return field.auto_now is True or field.auto_now_add is True
@@ -40,20 +63,36 @@ class RevisionOptions(object):
         cls._revisions = self
         if self.options:
             options_attrs = self.options.__dict__.copy()
+
             for name in self.options.__dict__:
                 if name.startswith('_'):
                     del options_attrs[name]
+
             for attr_name in self.AVAILABLE_OPTIONS:
                 if attr_name in options_attrs:
                     if attr_name == 'fields' and\
                                     options_attrs['fields'] == '__all__':
-                        setattr(self, attr_name,
-                                [field.name for field in cls._meta.fields
-                                 if field.name != 'id'])
+                        setattr(
+                            self,
+                            attr_name,
+                            [
+                                field.name for field in cls._meta.fields
+                                if field.name not in ['id', 'is_head']
+                            ]
+                        )
                     else:
                         setattr(self, attr_name, options_attrs.pop(attr_name))
                 elif hasattr(self.options, attr_name):
                     setattr(self, attr_name, getattr(self.options, attr_name))
+        else:
+            setattr(
+                self,
+                'fields',
+                [
+                    field.name for field in cls._meta.fields
+                    if field.name not in ['id', 'is_head']
+                ]
+            )
 
 
 class RevisionBase(ModelBase):
@@ -65,24 +104,30 @@ class RevisionBase(ModelBase):
         new_class = super(RevisionBase, mcs).__new__(mcs, name, bases, attrs)
         new_class.add_to_class('_revisions', RevisionOptions(revisions_options))
 
-        revision_class = mcs._create_revisions_model(
-            name, bases, revision_attrs, new_class)
+        if new_class._meta.model_name != 'revisionmodel':
+            revision_class = mcs._create_revisions_model(
+                name, bases, revision_attrs, new_class)
+
+            revision_class.add_to_class(
+                'is_head', models.BooleanField(default=False))
+
+            mcs._add_serialize_fields(revision_class)
 
         if new_class._revisions.soft_deletion:
             new_class.add_to_class(
                 'is_deleted', models.BooleanField(default=False))
 
         mcs._add_serialize_fields(new_class)
-        mcs._add_serialize_fields(revision_class)
 
         return new_class
 
     @classmethod
     def _add_serialize_fields(mcs, cls):
         for field in cls._meta.fields:
-            exluded_field_names = ['revision_for']
+            exluded_field_names = ['revision_for', 'is_head']
             if isinstance(field, models.ForeignKey) and \
-                            field.name not in exluded_field_names:
+                    field.name not in exluded_field_names:
+
                 field.null = True
                 field.blank = True
                 field.rel.on_delete = revision_on_delete
@@ -99,23 +144,17 @@ class RevisionBase(ModelBase):
         new_attrs = {key: val for key, val in attrs.items()
                      if not isinstance(val, Field)}
 
-        revision_fields = cls._revisions.fields
-        if type(revision_fields) == str and revision_fields == '__all__':
-            revision_fields = {
-                key: val for key, val in attrs.items()
-                if isinstance(val, Field) and not auto_now_field(val)
-            }
-        elif type(revision_fields) == list:
-            revision_fields = {key: attrs[key] for key in revision_fields}
+        revision_fields = {key: attrs[key] for key in cls._revisions.fields}
 
         new_attrs.update(revision_fields)
         new_class_name = name + 'Revision'
         new_attrs['__qualname__'] = new_class_name
 
+        bases = (Revision,)
         revision_class = super_new(mcs, new_class_name, bases, new_attrs)
         revision_class.add_to_class(
             'revision_for',
-            models.ForeignKey(cls)
+            models.ForeignKey(cls, related_name='revisions'),
         )
         revision_class.add_to_class(
             'revision_at',
@@ -138,7 +177,7 @@ class RevisionModel(six.with_metaclass(RevisionBase, models.Model)):
         abstract = True
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(RevisionModel, self).__init__(*args, **kwargs)
         self.old_data = self._get_instance_data()
 
     def _get_instance_data(self):
@@ -159,31 +198,42 @@ class RevisionModel(six.with_metaclass(RevisionBase, models.Model)):
                 data[field_name] = field.value_from_object(self)
         return data
 
+    def _create_revision(self):
+        self.revision_class.objects.create(
+            revision_for=self,
+            parent_revision=self.current_revision,
+            **self.old_data
+        )
+
     def save(self, *args, **kwargs):
+
+        # TODO: We should also do the things below when using `update()`
+
         new_data = self._get_instance_data()
         has_changed = self.old_data != new_data
 
         if kwargs.pop('soft_deletion', None):
             self.is_deleted = True
 
-        if self.pk and has_changed:
-            parent_revision = self.revision_class.objects.last()
-            self.revision_class.objects.create(
-                revision_for=self,
-                parent_revision=parent_revision,
-                **self.old_data
-            )
-
         # Update the old data dict so that a revision does not get created
         # if an instance gets saved multiple times without changes
         self.old_data = self._get_instance_data()
         super().save(*args, **kwargs)
+
+        self._create_revision()
 
     def delete(self, using=None):
         if self._revisions.soft_deletion:
             self.save(using=using, soft_deletion=True)
         else:
             super(RevisionModel, self).delete(using=using)
+
+    @property
+    def current_revision(self):
+        try:
+            return self.revisions.get(is_head=True)
+        except self.revision_class.DoesNotExist:
+            return None
 
     def _field_diff(self, other, field):
 
--- testproject/tests/test_revisionmodel.py
+++ testproject/tests/test_revisionmodel.py
@@ -4,63 +4,54 @@ from contextlib import contextmanager
 from testapp import models
 
 import pytest
-import loremipsum
 
 
 def test_revision_on_edit(db):
     bar1 = models.Bar.objects.create()
-    assert bar1.revision_class.objects.count() == 0
+    assert bar1.id
+    assert bar1.revision_class.objects.count() == 1
 
     bar1.char = 'Bar1'
     bar1.save()
+    assert bar1.revision_class.objects.count() == 2
 
-    assert bar1.revision_class.objects.count() == 1
-
-    revision1 = bar1.revision_class.objects.last()
-    assert revision1.char is None
-
-    text_body = loremipsum.generate_paragraph()
+    text_body = 'lalala'
     bar1.char = 'Bar1 updated'
     bar1.text = text_body
     bar1.save()
 
-    assert bar1.revision_class.objects.count() == 2
+    assert bar1.revision_class.objects.count() == 3
 
     revision2 = bar1.revision_class.objects.last()
-    assert revision2.text is None
-
-    # bar1.save()
-    # bar1.save()
-    # bar1.save()
-
-    assert bar1.revision_class.objects.count() == 2
-
+    assert revision2.text == text_body
 
 def test_foreign_keys(db):
     non_revisioned_instance = models.NonRevisionedModel.objects.create()
     bar1 = models.Bar.objects.create(
         non_revisioned_foreign_key=non_revisioned_instance
     )
-    assert bar1.revision_class.objects.count() == 0
+    assert bar1.revision_class.objects.count() == 1
 
     bar1.non_revisioned_foreign_key = None
     bar1.save()
-    assert bar1.revision_class.objects.count() == 1
+    assert bar1.revision_class.objects.count() == 2
 
     revision1 = bar1.revision_class.objects.first()
     assert revision1.non_revisioned_foreign_key == non_revisioned_instance
 
     non_revisioned_instance.delete()
 
-    assert bar1.revision_class.objects.count() == 1
+    assert bar1.revision_class.objects.count() == 2
 
 
 def test_parent_revision(db):
     bar1 = models.Bar.objects.create()
+
+    first_revision = bar1.current_revision
+    assert first_revision.parent_revision is None
+
     bar1.char = 'foo'
     bar1.save()
-    first_revision = bar1.revision_class.objects.last()
-    assert first_revision.parent_revision is None
-    bar1.text = 'baz'
-    bar1.save()
-    assert bar1.revision_class.objects.last().parent_revision == first_revision
+
+    assert bar1.current_revision != first_revision
+    assert bar1.current_revision.parent_revision == first_revision
