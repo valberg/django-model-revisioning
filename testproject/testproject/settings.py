48232d90f7d0617f52e46db675e4c0f50512dcd7
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-18 17:46:38 +0100

    Travis CI

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'testapp',
 )
 
@@ -59,8 +60,10 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'docbrown',
+        'USER': 'docbrown',
+        'PASSWORD': 'docbrown',
     }
 }
 
