4ce60a5b764395c59dc4539b445b52d5f9065888
Vidir Valberg Gudmundsson <valberg@orn.li>
2019-10-17 21:52:33 +0200

    Make sure only valid options are given to the RevisionsOptions class. Also be fancy and do a suggestion if the non-valid option is close to a valid one.

--- model_revisioning/options.py
+++ model_revisioning/options.py
@@ -1,3 +1,7 @@
+from difflib import SequenceMatcher
+
+from django.core.exceptions import ImproperlyConfigured
+
 from model_revisioning.fields import RevisionedForeignKey
 
 
@@ -28,8 +32,32 @@ class RevisionOptions(object):
             options_attrs = self.options.__dict__.copy()
 
             for name in self.options.__dict__:
+                # Get rid of __module__, __dict__, etc.
                 if name.startswith("_"):
                     del options_attrs[name]
+                    continue
+
+                if name not in self.AVAILABLE_OPTIONS:
+                    # If the given option is not one that is available
+                    # we want to guide the user as best as possible.
+                    # So we check if the name of the given option is
+                    # similar to the ones available, and if so we suggest
+                    # the closest match!
+                    best_match = None
+                    best_match_ratio = 0.7
+                    for option in self.AVAILABLE_OPTIONS:
+                        ratio = SequenceMatcher(a=name, b=option).ratio()
+                        if ratio > best_match_ratio:
+                            best_match = option
+                            best_match_ratio = ratio
+
+                    did_you_mean = ""
+                    if best_match:
+                        did_you_mean = f' Did you mean "{best_match}"?'
+
+                    raise ImproperlyConfigured(
+                        f'"{name}" is not a valid option for Revisions.{did_you_mean}'
+                    )
 
             for attr_name in self.AVAILABLE_OPTIONS:
                 if attr_name in options_attrs:
